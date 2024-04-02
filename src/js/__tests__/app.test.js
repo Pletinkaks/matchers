de7e534dae1e059c40ce8bdcef41c5bd13bd3932
Ksenia Pletinka <pletinkaksenia@gmail.com>
2024-04-02 22:02:46 +0300

    Initial commit

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,32 @@
+import sorter from '../app';
+
+const characterInfo1 = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
+
+const characterInfo2 = [
+  { name: 'мечник', health: 100 },
+  { name: 'маг', health: 10 },
+  { name: 'лучник', health: 80 },
+];
+
+const characterInfo3 = [
+  { name: 'мечник', health: 100 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
+
+
+test('sorting characterInfo1', () => {
+  expect(characterInfo1.sort(sorter)).toEqual(characterInfo1);
+});
+
+test('sorting characterInfo2', () => {
+  expect(characterInfo2.sort(sorter)).toEqual(characterInfo2);
+});
+
+test('sorting characterInfo3', () => {
+  expect(characterInfo3.sort(sorter)).toEqual(characterInfo3);
+});
---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+function sorter(a, b) {
+    if (a.health > b.health) {
+      return 1;
+    }
+    if (a.health < b.health) {
+      return -1;
+    }
+    return 0;
+  }
+  
+  export default sorter;
